f3f6a047dc82a64807035f15be14ecb8b5f0bfb3
apasny <apasov157@gmail.com>
2021-11-22 20:48:21 +0300

    initial commit

---
+++ src/main/java/com/epam/calculator/Calculator.java
@@ -0,0 +1,20 @@
+package com.epam.calculator;
+
+public class Calculator {
+
+    public double add (double firstArgument, double secondArgument){
+        return firstArgument+secondArgument;
+    }
+
+    public double multiply(double firstArgument, double secondArgument) {
+        return firstArgument*secondArgument;
+    }
+
+    public double subtract(double firstArgument, double secondArgument) {
+        return firstArgument-secondArgument;
+    }
+
+    public double divide(double firstArgument, double secondArgument) {
+        return firstArgument/secondArgument;
+    }
+}
---
+++ src/test/java/com/epam/calculator/CalculatorTest.java
@@ -0,0 +1,102 @@
+package com.epam.calculator;
+
+import org.apache.commons.math3.util.Precision;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    public final double FIRST_ARGUMENT_POSITIVE = 1.1;
+    public final double SECOND_ARGUMENT_POSITIVE = 3.1;
+    public final double FIRST_ARGUMENT_NEGATIVE = -1.1;
+    public final double SECOND_ARGUMENT_NEGATIVE = -3.1;
+
+    @Test
+    public void testAddShouldAddWhenNumbersPositive() {
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        double result = calculator.add(FIRST_ARGUMENT_POSITIVE,SECOND_ARGUMENT_POSITIVE);
+
+        //then
+        Assertions.assertEquals(4.2, result);
+    }
+    @Test
+    public void testAddShouldAddWhenNumbersNegative() {
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        double result = calculator.add(FIRST_ARGUMENT_NEGATIVE,SECOND_ARGUMENT_NEGATIVE);
+
+        //then
+        Assertions.assertEquals(-4.2, result);
+    }
+    @Test
+    public void testMultiplyShouldMultiplyWhenNumbersPositive() {
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        double result = calculator.multiply(FIRST_ARGUMENT_POSITIVE,SECOND_ARGUMENT_POSITIVE);
+
+        //then
+        Assertions.assertEquals(3.41, Precision.round(result,8));
+    }
+    @Test
+    public void testMultiplyShouldMultiplyWhenNumbersNegative() {
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        double result = calculator.multiply(FIRST_ARGUMENT_NEGATIVE,SECOND_ARGUMENT_NEGATIVE);
+
+        //then
+        Assertions.assertEquals(3.41, Precision.round(result,8));
+    }
+    @Test
+    public void testSubtractShouldSubtractWhenNumbersPositive() {
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        double result = calculator.subtract(FIRST_ARGUMENT_POSITIVE,SECOND_ARGUMENT_POSITIVE);
+
+        //then
+        Assertions.assertEquals(-2, result);
+    }
+    @Test
+    public void testSubtractShouldAddWhenNumbersNegative() {
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        double result = calculator.subtract(FIRST_ARGUMENT_NEGATIVE,SECOND_ARGUMENT_NEGATIVE);
+
+        //then
+        Assertions.assertEquals(2, result);
+    }
+    @Test
+    public void testDivideShouldDivideWhenNumbersPositive() {
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        double result = calculator.divide(FIRST_ARGUMENT_POSITIVE,SECOND_ARGUMENT_POSITIVE);
+
+        //then
+        Assertions.assertEquals(0.3548387096774194, result);
+    }
+    @Test
+    public void testDivideShouldDivideWhenNumbersNegative() {
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        double result = calculator.divide(FIRST_ARGUMENT_NEGATIVE,SECOND_ARGUMENT_NEGATIVE);
+
+        //then
+        Assertions.assertEquals(0.3548387096774194, result);
+    }
+}
